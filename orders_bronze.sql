0c962622c67e37596bf508f3c9416c098ce599aa
Abdulrahman Alharbi <43651974+Abdulrahman-Al-Harbi@users.noreply.github.com>
2026-08-13 21:01:37 +0300

    Delete orders_bronze.sql

--- orders_bronze.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REFRESH STREAMING TABLE orders_bronze
-AS
-SELECT *
-FROM STREAM read_files(
-    '/Volumes/workspace/default/olist_raw/',
-    format => 'csv',
-    header => true,
-    inferSchema => true
-);
